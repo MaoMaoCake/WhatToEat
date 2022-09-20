97e583291651e4cb937ca3a705ac1aa19c3b1712
maomao <tangpansak@gmail.com>
2022-09-20 21:59:07 +0700

    simple randomiser code

---
+++ src/routes/random/Randomiser.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+    let results = "Nothing Yet";
+    // todo possibilities should come from a database
+    let possibilities: string[] = ["Thai Food", "Japanese Food", "Western Food"];
+
+    // choosing function
+    function choose(choices: string[]): string {
+        let index = Math.floor(Math.random() * choices.length);
+        return choices[index];
+    }
+
+    function onClick(): void{
+        results = choose(possibilities);
+
+    }
+</script>
+
+<p class="text-primary">You Got: {results}</p>
+<p class="btn btn-primary" on:click={onClick}>Click To Get A Random Place</p>
