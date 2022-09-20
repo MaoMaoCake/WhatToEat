449b0187087118c77b20b0013d3a9060de4a8f92
maomao <tangpansak@gmail.com>
2022-09-20 21:43:54 +0700

    now highlight works with subpages

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -26,7 +26,7 @@
         {#each menus as menu}
           {#if menu.type === "dropdown"}
             <li tabindex="0">
-                <a href={menu.link} class:text-secondary={$page.url.pathname === menu.link}>
+                <a href={menu.link} class:text-secondary={$page.url.pathname === menu.link || $page.url.pathname.includes(menu.link)}>
                 {menu.name}
                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
               </a>
@@ -49,7 +49,7 @@
       {#each menus as menu}
         {#if menu.type === "dropdown"}
           <li tabindex="0">
-            <a href={menu.link} class:text-secondary={$page.url.pathname === menu.link}>
+            <a href={menu.link} class:text-secondary={$page.url.pathname === menu.link || $page.url.pathname.includes(menu.link)}>
               {menu.name}
               <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
             </a>
