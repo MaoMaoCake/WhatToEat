62664d0b9d9824fcb341fd7659ac0bbc5060d5f0
maomao <tangpansak@gmail.com>
2022-09-25 18:48:47 +0700

    CTA for about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import Hero from "./Hero.svelte";
+    import CTA from "./CTA.svelte";
 </script>
 
 <Hero/>
+<CTA/>
---
+++ src/routes/about/CTA.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+
+</script>
+<div class="hero min-h-screen">
+    <div class="hero-content text-center text-neutral-content">
+        <div id="get_started">
+            <div class="p-5">
+                <p class="text-neutral-content text-4xl p-2">What is What To Eat?</p>
+                <p>WTE is a AI powered restaurant recommendation app</p>
+                <p>We take your current location (with your permission!) and your past history, to recommend a restaurant that you will like!</p>
+                <p>The more you use it the better it gets</p>
+            </div>
+            <div class="flex justify-around pt-10">
+                <div class="card w-96 bg-base-100 shadow-xl image-full">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Get A Random Store</h2>
+                        <p>Click to go to our Recommendation Page</p>
+                        <div class="card-actions justify-end">
+                            <a href="/random" class="btn btn-primary">Let's Go!</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl image-full">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Explore</h2>
+                        <p>Explore Our Collection</p>
+                        <div class="card-actions justify-end">
+                            <a href="/categories" class="btn btn-primary">Let's Go!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
